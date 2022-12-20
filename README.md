27840f956afb73ea59053e8d8115350aa1d63987
kovalen1618 <kovalen1.618@gmail.com>
2022-12-20 03:45:35 -0600

    Created login page with authentication
    Created isAuth state to login user with Google authentication
    Allowed for local storage of user once authenticated
    Implemented logout feature to allow user to reset state and local storage

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ---------- React ----------
 
-What is react-router-dom?
+_react-router-dom_ is an npm package that enables you to implement page routing in a web app so that users can easily navigate between and view the pages.
+
+It is used for single-page applications: apps that do in fact have many pages or components but are never refreshed - instead their content is dynamically fetched based on the URL through the process of Routing - made possible with the help of react-router-dom.
 
 <code>npm install react-router-dom</code>
+
+---------- Local Storage ----------
+
+How does local storage work?
--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
 
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
+
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+
+  // Resets state, clears local storage, and redirects user to login
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = '/login';
+    });
+  }
+
   return (
     <Router>
       {/* Navbar with links to different routes */}
+      {/* You are not allowed to use anything from the react-router-dom outside of the <Router> */}
       <nav>
         <Link to='/'> Home </Link>
         <Link to='/createpost'> CreatePost </Link>
-        <Link to='/login'> Login </Link>
+        {/* Will only show the Login link on the Navbar if user is not logged-in */}
+        {!isAuth ? <Link to='/login'> Login </Link> : <button onClick={signUserOut}> Log Out </button>}
       </nav>
 
       {/* Define structure of the Routes */}
@@ -20,7 +38,7 @@ function App() {
         {/* Home page route */}
         <Route path='/' element={<Home />} />
         <Route path='/createpost' element={<CreatePost />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
   )
